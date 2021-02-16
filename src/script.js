c081dd780e7bb3bac849fc93971421004103a3b9
Thais <thais.map@hotmail.com>
2021-02-16 17:34:40 -0300

    salva e atualiza arquivo com pastas selecionadas

--- src/script.js
+++ src/script.js
@@ -3,8 +3,8 @@ const alumna = require('@alumna/reflect');
 
 let ulista = document.querySelector('#pastasSelecionadas');
 let divSelecao = document.querySelector('#selecao');
+let log = document.querySelector('#txtLog>p');
 
-divSelecao.style.display = 'none';
 let pastas = [];
 
 function addPasta() {
@@ -19,28 +19,38 @@ function addPasta() {
         console.error('Pasta repetida');
       } else {
         pastas.push(valor);
-        exibirNaTela();
+        exibirPastas();
       }
     }
   });
 }
 
-function exibirNaTela() {
+function exibirPastas() {
   ulista.innerHTML = '';
-  divSelecao.style.display = 'inline';
 
-  pastas.forEach((pasta) => {
-    let item = document.createElement('option');
-    item.innerHTML = pasta;
-    ulista.append(item);
-  });
+  if (pastas.length > 0) {
+    divSelecao.style.display = 'inline';
+
+    pastas.forEach((pasta) => {
+      let item = document.createElement('option');
+      item.innerHTML = pasta;
+      ulista.append(item);
+    });
+  } else {
+    divSelecao.style.display = 'none';
+  }
 }
 
 function copiar() {
+  log.innerHTML = '';
+  incluirLog('*** Backup Iniciado ***');
   pastas.forEach((pasta) => {
-    console.log(`copiando pasta ${pasta}`);
     fazerBackup(pasta);
   });
+}
+
+function incluirLog(texto) {
+  log.innerText += texto + '\n';
 }
 
 async function fazerBackup(folderName) {
@@ -67,11 +77,31 @@ async function fazerBackup(folderName) {
     // exclude: ['skip-this-file.txt', 'skip/this/directory'],
   });
 
-  if (err) console.error(err);
-  else console.log(res); // Directory "src/" reflected to "dest/"
+  if (err) incluirLog(err);
+  else
+    incluirLog(
+      res.replace('Directory', 'Pasta').replace('reflected to', 'copiada para'),
+    );
 }
 
 function remover() {
-  pastas.delete(ulista.value);
-  exibirNaTela();
+  pastas.splice(ulista.selectedIndex, 1);
+  exibirPastas();
+}
+
+function carregar() {
+  fs.readFile('./config/sources.json', 'utf8', (err, data) => {
+    if (err) throw err;
+    pastas = JSON.parse(data).paths;
+    exibirPastas();
+  });
+}
+
+function salvar() {
+  let obj = { paths: pastas };
+  let json = JSON.stringify(obj);
+  fs.writeFile('./config/sources.json', json, (err) => {
+    if (err) throw err;
+    alert('Configuração de pastas salva!');
+  });
 }
